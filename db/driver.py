192cd5517e82501d98bc9cd3515f165fec587e37
xin.xiao@hotmail.com <xin.xiao@hotmail.com>
2017-09-30 01:45:28 +0000

    [DB-Driver] Regulate methods by using private fields and methods. Shorten the name of some method

--- db/driver.py
+++ db/driver.py
@@ -7,42 +7,39 @@ class Driver:
   def __init__(self, host='172.17.0.1', port=27017, 
                      db='dg', collection='sketch'):
     url = DB_URL_LOCALE.format(host, port)
-    self.collection = MongoClient(url)[db][collection]
-    self._cache = {}
+    self.__collection = MongoClient(url)[db][collection]
+    self.__cache = {}
 
-  def cached(function):
+  def __cached(function):
     def cached_function(*argv, **kw):
-      cache = argv[0]._cache
+      cache = argv[0].__cache
       token = tuple([function.__name__] + list(argv[1:]))
       if token not in cache:
         cache[token] = function(*argv, **kw)
       return cache[token]
     return cached_function
 
-  @cached
-  def get_total_size(self):
-    return self.collection.find().count()
+  @__cached
+  def total_size(self):
+    return self.__collection.find().count()
 
-  @cached
-  def get_list_of_catagories(self):
-    return self.collection.distinct('word')
+  @__cached
+  def catagory_list(self):
+    return self.__collection.distinct('word')
 
-  @cached
-  def get_size_by_catagory(self, catagory):
-    return self.collection.find({ 'word': catagory }).count()
+  @__cached
+  def size_of_catagory(self, catagory):
+    return self.__collection.find({ 'word': catagory }).count()
 
-  @cached
-  def get_one_by_id(self, object_id):
-    return self.collection.find_one({ '_id': ObjectId(object_id) })
+  @__cached
+  def find_by_id(self, object_id):
+    return self.__collection.find_one({ '_id': ObjectId(object_id) })
 
-  def get_data_of_multiple_catagories(self, catagory_list):
-    return self.collection.find({ 'word': { '$in': catagory_list } })
+  def find_multiple_catagories(self, catagory_list):
+    return self.__collection.find({ 'word': { '$in': catagory_list } })
   
-  def get_data_of_catagory(self, catagory):
-    return self.collection.find({ 'word': catagory })
+  def find_catagory(self, catagory):
+    return self.__collection.find({ 'word': catagory })
 
-  def get_all_data(self):
-    return self.collection.find()
-
-  def insert_one(self, data):
-    self.collection.insert_one(data)
+  def find_all(self):
+    return self.__collection.find()
